cd79d47cf28b5e99225672be0a23ea215f9c235e
Lucdomingues <lojalucc@outlook.com>
2023-05-26 12:31:21 -0300

    [FIX]-produtos-e-etiqueta-google

--- src/Components/AboutMe.jsx
+++ src/Components/AboutMe.jsx
@@ -10,7 +10,18 @@ function AboutMe() {
         </div>
         <div className="about-me__content">
           <h2 className="about-me__title font_especial">Sobre a Daniela</h2>
-          <p className="about-me__description font_items">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla dapibus placerat rutrum. Aenean eget ultrices.  ut ligula mi. Nulla ut ris lorem. Nam hendrerit eleifend libero, non iaculis sapien semper nec.</p>
+          <p className="about-me__description font_price">Sou a Daniela Camargo, e é um prazer imenso me apresentar a vocês. Há 13 anos, mergulhei de cabeça no maravilhoso mundo dos bem casados e desde então, tenho vivido um verdadeiro conto de fadas açucarado. Ah, como amo o que faço! <br /> <br />
+      
+            Além de ser uma confeiteira de coração, sou mãe de dois filhos adoráveis, que são minha maior inspiração. Eles trazem luz e doçura à minha vida, assim como os docinhos que preparo com tanto carinho. <br /> <br />
+
+            Sabe, eu realmente me esforço para criar bem casados que sejam verdadeiras obras de arte, cheias de amor em cada detalhe. Cada mordida deve ser uma experiência mágica, com uma textura macia e um sabor que te leva para as nuvens. Tenho certeza de que cada um dos meus bem casados irá te fazer sorrir e suspirar de felicidade. <br /> <br />
+          
+            Se você está planejando seu casamento dos sonhos ou qualquer outro evento especial, estou aqui para torná-lo ainda mais mágico. Os meus bem casados são perfeitos para encantar seus convidados e criar memórias doces que durarão para sempre. <br /> <br />
+
+            Espero ansiosamente por sua visita! Venha me conhecer, deixe-se encantar pelo meu mundo açucarado e juntos vamos tornar seus sonhos em realidade. <br /> <br />
+
+            Com todo o meu carinho, <br />
+            Daniela Camargo</p>
           <div>
             <a href="https://wa.me/5511947319372" target="blank" className="about_link font_especial">Me chame!</a>
           </div>
--- src/Components/Galery.jsx
+++ src/Components/Galery.jsx
@@ -14,7 +14,7 @@ function Galery() {
                         <img src="images/IMG-20230515-WA0110.jpg" alt="Imagem 1" className="w-full h-64 object-cover" />
                         <div className="p-4">
                             <h2 className="text-xl font-bold mb-2 font_items">Tradicional</h2>
-                            <p className="text-gray-700 font_especial tam_font">Tradicional, na fita de cetim, embrulhado no crepom e no celofane.</p>
+                            <p className="text-gray-700 font_especial tam_font">Bem casado tradicional(doce de leite), papel crepom e fita de cetim</p>
                             <p className="font_price">R$ 2.99</p>
                         </div>
                     </div>
@@ -22,23 +22,23 @@ function Galery() {
                         <img src="images/IMG-20230515-WA0099.jpg" alt="Imagem 2" className="w-full h-64 object-cover" />
                         <div className="p-4">
                             <h2 className="text-xl font-bold mb-2 font_items">Tradicional</h2>
-                            <p className="text-gray-700 font_especial tam_font">Tradicional, na fita de cetim, embrulhado no crepom e no celofane.</p>
-                            <p className="font_price">R$ 2.99</p>
+                            <p className="text-gray-700 font_especial tam_font">Bem casado tradicional(doce de leite), papel crepom, fita de cetim e flor</p>
+                            <p className="font_price">R$ 3.99</p>
                         </div>
                     </div>
                     <div className="bg-white rounded overflow-hidden">
                         <img src="images/IMG-20230515-WA0067.jpg" alt="Imagem 3" className="w-full h-64 object-cover" />
                         <div className="p-4">
                             <h2 className="text-xl font-bold mb-2 font_items">Tradicional</h2>
-                            <p className="text-gray-700 font_especial tam_font">Tradicional, na fita de cetim, embrulhado no crepom e no celofane.</p>
-                            <p className="font_price">R$ 2.99</p>
+                            <p className="text-gray-700 font_especial tam_font">Bem casado tradicional(doce de leite), papel crepom e fita de cetim grossa</p>
+                            <p className="font_price">R$ 3.99</p>
                         </div>
                     </div>
                     <div className="bg-white rounded overflow-hidden">
                         <img src="images/IMG-20230515-WA0085.jpg" alt="Imagem 4" className="w-full h-64 object-cover" />
                         <div className="p-4">
                             <h2 className="text-xl font-bold mb-2 font_items">Tradicional</h2>
-                            <p className="text-gray-700 font_especial tam_font">Tradicional, na fita de cetim, embrulhado no crepom e no celofane.</p>
+                            <p className="text-gray-700 font_especial tam_font">Bem casado tradicional(doce de leite), papel crepom e fita de cetim</p>
                             <p className="font_price">R$ 2.99</p>
                         </div>
                     </div>
@@ -50,32 +50,32 @@ function Galery() {
                         <img src="images/IMG-20230515-WA0092.jpg" alt="Imagem 1" className="w-full h-64 object-cover" />
                         <div className="p-4">
                             <h2 className="text-xl font-bold mb-2 font_items">Tradicional</h2>
-                            <p className="text-gray-700 font_especial tam_font">Tradicional, na fita de cetim, embrulhado no crepom e no celofane.</p>
-                            <p className="font_price">R$ 2.99</p>
+                            <p className="text-gray-700 font_especial tam_font">Bem casado tradicional(doce de leite), tecido e fita cordão</p>
+                            <p className="font_price">R$ 3.99</p>
                         </div>
                     </div>
                     <div className="bg-white rounded overflow-hidden">
                         <img src="images/IMG-20230515-WA0087.jpg" alt="Imagem 2" className="w-full h-64 object-cover" />
                         <div className="p-4">
                             <h2 className="text-xl font-bold mb-2 font_items">Tradicional</h2>
-                            <p className="text-gray-700 font_especial tam_font">Tradicional, na fita de cetim, embrulhado no crepom e no celofane.</p>
-                            <p className="font_price">R$ 2.99</p>
+                            <p className="text-gray-700 font_especial tam_font">Bem casado tradicional(doce de leite), tecido e fita cetim</p>
+                            <p className="font_price">R$ 3.99</p>
                         </div>
                     </div>
                     <div className="bg-white rounded overflow-hidden">
                         <img src="images/IMG-20230515-WA0084.jpg" alt="Imagem 3" className="w-full h-64 object-cover" />
                         <div className="p-4">
                             <h2 className="text-xl font-bold mb-2 font_items">Tradicional</h2>
-                            <p className="text-gray-700 font_especial tam_font">Tradicional, na fita de cetim, embrulhado no crepom e no celofane.</p>
-                            <p className="font_price">R$ 2.99</p>
+                            <p className="text-gray-700 font_especial tam_font">Bem casado tradicional(doce de leite), tecido e renda</p>
+                            <p className="font_price">R$ 3.99</p>
                         </div>
                     </div>
                     <div className="bg-white rounded overflow-hidden">
                         <img src="images/IMG-20230515-WA0090.jpg" alt="Imagem 4" className="w-full h-64 object-cover" />
                         <div className="p-4">
                             <h2 className="text-xl font-bold mb-2 font_items">Tradicional</h2>
-                            <p className="text-gray-700 font_especial tam_font">Tradicional, na fita de cetim, embrulhado no crepom e no celofane.</p>
-                            <p className="font_price">R$ 2.99</p>
+                            <p className="text-gray-700 font_especial tam_font">Bem casado tradicional(doce de leite), papel crepom e fita de cetim decorada</p>
+                            <p className="font_price">R$ 3.99</p>
                         </div>
                     </div>
                 </div>
